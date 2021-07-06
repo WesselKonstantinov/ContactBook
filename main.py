bddfd4e9185e701ecdd006a5c37da65aba5c7621
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-07-06 23:39:41 +0200

    Add functionality for viewing contact details

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 import click
-
+from prettytable import from_db_cursor
 
 # Database setup
 connection = sqlite3.connect('contact_book.db')
@@ -38,7 +38,20 @@ def add(name, phone_number, email_address):
     click.echo(f'Saved contact details of {name}.')
 
 
+@click.command()
+@click.option('-i', '--id', help='Contact id', type=int)
+def view(id):
+    """View contact details of either one person or all people."""
+    if id:
+        cursor.execute('SELECT * FROM contacts WHERE contact_id = ?', (id,))
+    else:
+        cursor.execute('SELECT * FROM contacts')
+    contacts_table = from_db_cursor(cursor)
+    click.echo(contacts_table)
+
+
 cli.add_command(add)
+cli.add_command(view)
 
 if __name__ == '__main__':
     setup_contacts_table()
