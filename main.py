d7b298177005d36e9d9a2dec23f14dc9df61ae0c
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-07-07 11:54:16 +0200

    Add functionality for updating and deleting contact details

--- main.py
+++ main.py
@@ -39,19 +39,71 @@ def add(name, phone_number, email_address):
 
 
 @click.command()
-@click.option('-i', '--id', help='Contact id', type=int)
+@click.option('-i', '--id', help='Contact id of a given person', type=int)
 def view(id):
     """View contact details of either one person or all people."""
     if id:
         cursor.execute('SELECT * FROM contacts WHERE contact_id = ?', (id,))
     else:
         cursor.execute('SELECT * FROM contacts')
+
     contacts_table = from_db_cursor(cursor)
     click.echo(contacts_table)
+
+    connection.close()
+
+
+@click.command()
+@click.argument('id', type=int)
+@click.option('-n', '--name', help='Name of a given person')
+@click.option('-pn', '--phone-number', help='Phone number of a given person')
+@click.option('-ea', '--email-address', help='Email address of a given person')
+def edit(id, name, phone_number, email_address):
+    """Edit contact details of a given person."""
+    cursor.execute('SELECT * FROM contacts WHERE contact_id = ?', (id,))
+    contact = cursor.fetchone()
+    if contact:
+        if name:
+            cursor.execute(
+                'UPDATE contacts SET name = ? WHERE contact_id = ?',
+                (name, id)
+            )
+        if phone_number:
+            cursor.execute(
+                'UPDATE contacts SET phone_number = ? WHERE contact_id = ?',
+                (phone_number, id)
+            )
+        if email_address:
+            cursor.execute(
+                'UPDATE contacts SET email_address = ? WHERE contact_id = ?',
+                (email_address, id)
+            )
+        connection.commit()
+        connection.close()
+        click.echo('Successfully updated contact details.')
+    else:
+        click.echo('Contact not found.')
+
+
+@click.command()
+@click.argument('id', type=int)
+def delete(id):
+    """Delete contact details of a given person."""
+    cursor.execute('SELECT * FROM contacts WHERE contact_id = ?', (id,))
+    contact = cursor.fetchone()
+    if contact:
+        cursor.execute('DELETE FROM contacts WHERE contact_id = ?', (id,))
+        connection.commit()
+        connection.close()
+        click.echo('Successfully deleted contact.')
+    else:
+        click.echo('Contact not found.')
 
 
 cli.add_command(add)
 cli.add_command(view)
+cli.add_command(edit)
+cli.add_command(delete)
 
 if __name__ == '__main__':
     setup_contacts_table()
