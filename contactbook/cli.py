0ec8c80a6c8afbedbdd0512685a0cc62eabd451c
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-07-12 15:46:00 +0200

    Add -h flag to context settings

--- contactbook/cli.py
+++ contactbook/cli.py
@@ -4,7 +4,10 @@ from . import db
 
 
 # Command-line setup
-@click.group()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
     pass
 
